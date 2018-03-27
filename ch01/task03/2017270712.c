c806c9b5d5448dd5a62dfef45a8210c873e4c5b4
jojejin <37167652+jojejin@users.noreply.github.com>
2018-03-27 11:52:35 +0900

    Create 2017270712.c

---
+++ ch01/task03/2017270712.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+typedef struct matrix{
+        int row, col;
+} matrix;
+
+typedef struct _elements {
+        int row;
+        int col;
+        int value;
+} element;
+
+typedef element* smatrix;
+
+
+void smadd(smatix a,smatix b, smatrix d){
+        int maxCols;
+        int Col[maoxCols]= {0};
+        for (int i=0 ; i < maxRow; i++)
+                for<F5>hn
+}
